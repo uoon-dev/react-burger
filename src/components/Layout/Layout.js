b91f64641e3717e0844a4770308eeee68408322d
uoon-dev <uoon.dev@gmail.com>
2018-10-18 12:02:21 +0900

    add Logo component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxiliary>
+        <Toolbar/>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
