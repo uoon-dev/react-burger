15a0d6a59cf47417c20a54304340ce9869df6daa
uoon-dev <uoon.dev@gmail.com>
2018-10-18 16:31:39 +0900

    add Toolbar and Logo components

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationTimes from '../NavigationItems/NavigationItems'; 
 
 const toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo/>
         <nav>
-            ...
+            <NavigationTimes/>
         </nav>
     </header>
 );
