4319e5b45c4035e8bf64a78b8682f2a4cb88e198
uoon-dev <uoon.dev@gmail.com>
2018-10-18 19:01:05 +0900

    add SideDrawer component for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxiliary>
-        <Toolbar/>
+        <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import NavigationTimes from '../NavigationItems/NavigationItems';
 const toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
         <nav>
             <NavigationTimes/>
         </nav>
