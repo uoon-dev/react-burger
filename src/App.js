d06ed98909832d86acac00113f6f30601f34a9f1
uoon-dev <uoon.dev@gmail.com>
2018-10-23 16:09:49 +0900

    make Checkout Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  state = {
-    show: true
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false});
-    }, 5000)
-  }
-
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
